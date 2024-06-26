80362ff2a9a1347472bc67108f563a111635dca7
Bertil Braun <bertil.braun.private@gmail.com>
2024-06-26 15:00:33 +0200

    Adjusted testing params

--- src/dpo_cluster/generate_samples.sh
+++ src/dpo_cluster/generate_samples.sh
@@ -2,13 +2,13 @@
 
 #SBATCH --job-name=generate                # job name
 #SBATCH --partition=gpu_8                  # mby GPU queue for the resource allocation.
-#SBATCH --time=03:00:00                    # wall-clock time limit
+#SBATCH --time=01:00:00                    # wall-clock time limit
 #SBATCH --mem=100000                       # memory per node
 #SBATCH --nodes=1                          # number of nodes to be used
 #SBATCH --cpus-per-task=8                  # number of CPUs required per MPI task
 #SBATCH --ntasks-per-node=1                # maximum count of tasks per node
 #SBATCH --mail-type=ALL                    # Notify user by email when certain event types occur.
-#SBATCH --gres=gpu:8
+#SBATCH --gres=gpu:2 # TODO 8
 #SBATCH --output=generate_%j.txt
 #SBATCH --error=generate_%j.txt
 
--- src/dpo_cluster/train_and_evaluate.sh
+++ src/dpo_cluster/train_and_evaluate.sh
@@ -2,13 +2,13 @@
 
 #SBATCH --job-name=train                   # job name
 #SBATCH --partition=gpu_4_a100             # mby GPU queue for the resource allocation.
-#SBATCH --time=02:30:00                    # wall-clock time limit # TODO: set to 08:00:00
+#SBATCH --time=02:00:00                    # wall-clock time limit # TODO: set to 08:00:00
 #SBATCH --mem=100000                       # memory per node
 #SBATCH --nodes=1                          # number of nodes to be used
 #SBATCH --cpus-per-task=1                  # number of CPUs required per MPI task
 #SBATCH --ntasks-per-node=1                # maximum count of tasks per node
 #SBATCH --mail-type=ALL                    # Notify user by email when certain event types occur.
-#SBATCH --gres=gpu:1
+#SBATCH --gres=gpu:1 # TODO can we train with more? accelerate 
 #SBATCH --output=train_%j.txt
 #SBATCH --error=train_%j.txt
 
