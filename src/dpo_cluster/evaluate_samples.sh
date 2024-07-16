65c5358e42357bfc961b698916068225a966d170
Bertil Braun <bertil.braun.private@gmail.com>
2024-07-16 12:27:58 +0200

    Update evaluate_samples.sh

--- src/dpo_cluster/evaluate_samples.sh
+++ src/dpo_cluster/evaluate_samples.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 #SBATCH --job-name=evaluate                # job name
-#SBATCH --partition=gpu_4_a100             # mby GPU queue for the resource allocation.
-#SBATCH --time=00:45:00  # TODO 3h?                  # wall-clock time limit
+#SBATCH --partition=dev_gpu_4_a100             # mby GPU queue for the resource allocation.
+#SBATCH --time=00:30:00  # TODO 3h?                  # wall-clock time limit
 #SBATCH --mem=100000                       # memory per node
 #SBATCH --nodes=1                          # number of nodes to be used
 #SBATCH --cpus-per-task=4                  # number of CPUs required per MPI task
 #SBATCH --ntasks-per-node=1                # maximum count of tasks per node
 #SBATCH --mail-type=ALL                    # Notify user by email when certain event types occur.
-#SBATCH --gres=gpu:2 # TODO 4
+#SBATCH --gres=gpu:4 # TODO 4
 #SBATCH --output=evaluate_%j.txt
 #SBATCH --error=evaluate_%j.txt
 
