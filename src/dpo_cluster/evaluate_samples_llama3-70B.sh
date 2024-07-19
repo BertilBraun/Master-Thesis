c21f52f6fdf891e85e332975e3a935f46c1161c1
Bertil Braun <bertil.braun.private@gmail.com>
2024-07-19 18:06:09 +0200

    Removed Transformers Evaluation for API Evaluation

--- src/dpo_cluster/evaluate_samples_llama3-70B.sh
+++ src/dpo_cluster/evaluate_samples_llama3-70B.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-#SBATCH --job-name=evaluate_openai         # job name
-#SBATCH --partition=multiple               # mby GPU queue for the resource allocation.
+#SBATCH --job-name=evaluate_llama3-70B         # job name
+#SBATCH --partition=single                 # mby GPU queue for the resource allocation.
 #SBATCH --time=02:00:00                    # wall-clock time limit
-#SBATCH --mem=100000                       # memory per node
+#SBATCH --mem=5000                         # memory per node
 #SBATCH --nodes=1                          # number of nodes to be used
-#SBATCH --cpus-per-task=20                 # number of CPUs required per MPI task
+#SBATCH --cpus-per-task=1                  # number of CPUs required per MPI task
 #SBATCH --ntasks-per-node=1                # maximum count of tasks per node
 #SBATCH --mail-type=ALL                    # Notify user by email when certain event types occur.
-#SBATCH --output=evaluate_openai_%j.txt
-#SBATCH --error=evaluate_openai_%j.txt
+#SBATCH --output=evaluate_llama3-70B_%j.txt
+#SBATCH --error=evaluate_llama3-70B_%j.txt
 
 source shared_slurm_setup.sh
 
-python -m src.dpo_cluster.evaluate_samples_openai
+python -m src.dpo_cluster.evaluate_samples_via_api dev-llama-large mlpc none
 
 # if the generate script is successful, then the next step is to train the model
 if [ $? -eq 0 ]; then
--- src/dpo_cluster/evaluate_samples_via_api.py
+++ src/dpo_cluster/evaluate_samples_via_api.py
@@ -1,15 +1,50 @@
-from concurrent.futures import ProcessPoolExecutor
-
+import sys
+from typing import Any
+from src.defines import LOCAL_AI_CODER, LOCAL_AI_ML_PC
 from src.language_model import OpenAILanguageModel
 from src.log import LogLevel, log
 from src.database import get_retriever_getter
 from src.evaluation import default_round_evaluator, get_all_preferences, prompt_for_ranking, run_tournament_ranking
-from src.types import EvaluationResult, EvaluationResult_from_invalid_response, Ranking
+from src.types import EvaluationResult, EvaluationResult_from_invalid_response, Ranking, TournamentNode
 from src.dpo_cluster.defines import *
 from src.util import create_backup, dump_json, json_dumper, load_json, log_all_exceptions, timeblock
 
+
 if __name__ == '__main__':
     START_DATETIME = get_previous_datetime_str()
+
+# if sysargs are not provided, exit with error message
+if len(sys.argv) < 4:
+    log(
+        'Please provide the Model ID and the base URL and which API KEY to use as command line arguments.',
+        level=LogLevel.ERROR,
+    )
+    exit(1)
+
+
+def parse_sys_arg(arg_name: str, arg_value: str, valid_values: dict[str, Any]) -> Any:
+    INVALID_ARG_VALUE = f'Invalid {arg_name} value'
+    value = valid_values.get(arg_value, INVALID_ARG_VALUE)
+    assert value != INVALID_ARG_VALUE, f'{INVALID_ARG_VALUE}: {arg_value}'
+    return value
+
+
+# Model to use from sysargs
+model_id = parse_sys_arg('Model ID', sys.argv[1], {'gpt-4o-mini': 'gpt-4o-mini', 'dev-llama-large': 'dev-llama-large'})
+
+# Base url from sysargs either None or LOCAL_AI_ML_PC
+base_url = parse_sys_arg('Base URL', sys.argv[2], {'openai': None, 'mlpc': LOCAL_AI_ML_PC, 'coder': LOCAL_AI_CODER})
+
+# API key to use from sysargs
+api_key = parse_sys_arg('API Key', sys.argv[3], {'cas': CAS_OPENAI_API_KEY, 'none': None})
+
+LLM = OpenAILanguageModel(
+    model_id,
+    debug_context_name='evaluate_samples_via_api',
+    base_url=base_url,
+    api_key=api_key,
+    max_retries=2,
+)
 
 
 def load_samples_to_evaluate() -> list[SampleToEvaluate]:
@@ -20,34 +55,27 @@ def load_samples_to_evaluate() -> list[SampleToEvaluate]:
 
 
 def evaluate_sample(sample_to_evaluate: SampleToEvaluate) -> list[PreferenceSample]:
-    with log_all_exceptions(f'evaluate for {sample_to_evaluate.author} failed'), timeblock(
-        f'Evaluating sample for {sample_to_evaluate.author}'
-    ):
-        return process_sample_to_evaluate(sample_to_evaluate)
+    with log_all_exceptions(f'evaluate for {sample_to_evaluate.author} failed'):
+        log(f'Processing sample to evaluate for {sample_to_evaluate.author}')
+        with timeblock(f'Evaluating sample for {sample_to_evaluate.author}'):
+            result = process_sample_to_evaluate(sample_to_evaluate)
+        log(f'Finished processing sample to evaluate for {sample_to_evaluate.author}')
+        return result
 
 
 def process_sample_to_evaluate(sample_to_evaluate: SampleToEvaluate) -> list[PreferenceSample]:
-    log(f'Processing sample to evaluate for {sample_to_evaluate.author}')
     examples = get_retriever_getter(max_number_to_retrieve=NUM_EXAMPLES)(Ranking).invoke(
         '\n\n'.join(sample_to_evaluate.abstracts)
     )
 
     def match_evaluator(profile1_index: int, profile2_index: int) -> EvaluationResult:
-        # TODO could be in parallel
         profile1 = sample_to_evaluate.profiles[profile1_index]
         profile2 = sample_to_evaluate.profiles[profile2_index]
 
         prompt = prompt_for_ranking(profile1, profile2, examples, sample_to_evaluate.abstracts)
 
-        llm = OpenAILanguageModel(
-            'gpt-4o',
-            debug_context_name='evaluate_samples_openai',
-            base_url=None,
-            api_key=CAS_OPENAI_API_KEY,
-            max_retries=2,
-        )
         log(f'Running match evaluator for {sample_to_evaluate.author} - {profile1_index} vs {profile2_index}')
-        response = llm.invoke(prompt, stop=['\n\n\n\n'], temperature=0.2)
+        response = LLM.invoke(prompt, stop=['\n\n\n\n'], temperature=0.2)
 
         # Log the response to a file
         dump_json(
@@ -69,6 +97,13 @@ def process_sample_to_evaluate(sample_to_evaluate: SampleToEvaluate) -> list[Pre
     )
     log(f'Tournament finished for {sample_to_evaluate.author}')
 
+    return tournament_to_preference_samples(sample_to_evaluate, tournament)
+
+
+def tournament_to_preference_samples(
+    sample_to_evaluate: SampleToEvaluate,
+    tournament: TournamentNode,
+) -> list[PreferenceSample]:
     preferences: list[PreferenceSample] = []
 
     for preference in get_all_preferences(tournament):
@@ -86,7 +121,7 @@ def process_sample_to_evaluate(sample_to_evaluate: SampleToEvaluate) -> list[Pre
     return preferences
 
 
-def compare_aggreement_of_preferences(preference_path: str, other_preference_path: str) -> tuple[float, int, int]:
+def calculate_aggreement_of_preferences(preference_path: str, other_preference_path: str) -> tuple[int, int]:
     # compare agreement with current preferences
     # map from prompt to (chosen, rejected)
     current_preferences: dict[str, tuple[str, str]] = {}
@@ -105,8 +140,7 @@ def compare_aggreement_of_preferences(preference_path: str, other_preference_pat
         number_of_samples += 1
         number_of_agreements += current_preferences[prompt] == (sample['chosen'], sample['rejected'])
 
-    percent = number_of_agreements / number_of_samples * 100
-    return percent, number_of_samples, number_of_agreements
+    return number_of_samples, number_of_agreements
 
 
 if __name__ == '__main__':
@@ -116,6 +150,22 @@ if __name__ == '__main__':
     if not preferences_existed:
         exit(1)
 
+    samples_to_evaluate = load_samples_to_evaluate()
+    log(f'Evaluating {len(samples_to_evaluate)} samples')
+
+    with json_dumper(preference_path) as dumper:
+        for sample in samples_to_evaluate:
+            for preference in evaluate_sample(sample):
+                dumper(preference)
+
+    # compare agreement with current preferences
+    number_of_samples, number_of_agreements = calculate_aggreement_of_preferences(
+        preference_path, preference_backup_path
+    )
+    log(
+        f'Agreement with current preferences: {number_of_agreements}/{number_of_samples} ({number_of_agreements / number_of_samples * 100:.2f}%)'
+    )
+
     # then write the samples with the current best model being the baseline model
     # - when the training with the new samples is done, then the current best model (which was trained on the preferences of llama3 70B) will be compared to the new model (trained on the preferences of GPT4o)
     all_samples = load_json(SAMPLES_FOR_FINE_TUNING_IMPROVEMENT_EVALUATION_FILE)
@@ -124,15 +174,3 @@ if __name__ == '__main__':
         for sample in all_samples:
             sample = SampleForFineTuningImprovementEvaluation.from_json(sample)
             dumper(sample.with_new_profiles(sample.best_profile_from_last_model, sample.best_profile_from_last_model))
-
-    samples_to_evaluate = load_samples_to_evaluate()
-    with ProcessPoolExecutor(max_workers=2) as executor, json_dumper(preference_path) as dumper:
-        for preferences in executor.map(evaluate_sample, samples_to_evaluate):
-            for preference in preferences:
-                dumper(preference)
-
-    # compare agreement with current preferences
-    percent, number_of_samples, number_of_agreements = compare_aggreement_of_preferences(
-        preference_path, preference_backup_path
-    )
-    log(f'Agreement with current preferences: {number_of_agreements}/{number_of_samples} ({percent:.2f}%)')
