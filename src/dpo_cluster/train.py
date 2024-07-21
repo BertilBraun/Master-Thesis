3f6c372fa03c98ee65568270b3f97fad2af403b5
Bertil Braun <bertil.braun.private@gmail.com>
2024-07-21 16:32:16 +0200

    Pad tokens

--- src/dpo_cluster/train.py
+++ src/dpo_cluster/train.py
@@ -58,6 +58,7 @@ def get_tokenizer(name_or_path: str = BASE_MODEL_ID) -> PreTrainedTokenizer | Pr
     )
 
     if tokenizer.pad_token_id is None:
+        tokenizer.pad_token = tokenizer.eos_token
         tokenizer.pad_token_id = tokenizer.eos_token_id
     if tokenizer.chat_template is None:
         tokenizer.chat_template = "{% for message in messages %}{{'<|' + message['role'] + '|> ' + message['content'] + '<|eos|>\n'}}{% endfor %}"
