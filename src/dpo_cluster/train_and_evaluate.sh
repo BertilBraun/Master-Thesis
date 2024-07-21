24eec5d8521271de6dedb0a0d7994508bef4a5b8
Bertil Braun <bertil.braun.private@gmail.com>
2024-07-21 15:56:48 +0200

    Update train_and_evaluate.sh

--- src/dpo_cluster/train_and_evaluate.sh
+++ src/dpo_cluster/train_and_evaluate.sh
@@ -2,13 +2,13 @@
 
 #SBATCH --job-name=train                   # job name
 #SBATCH --partition=gpu_4 # TODO does that work without gpu_4_a100?             # mby GPU queue for the resource allocation.
-#SBATCH --time=02:00:00              # TODO: set to 08:00:00      # wall-clock time limit 
+#SBATCH --time=02:00:00  # TODO: set to ~30sec * NUM_SAMPLES_TO_EVALUATE      # wall-clock time limit 
 #SBATCH --mem=100000                       # memory per node
 #SBATCH --nodes=1                          # number of nodes to be used
-#SBATCH --cpus-per-task=1                  # number of CPUs required per MPI task
+#SBATCH --cpus-per-task=4                  # number of CPUs required per MPI task
 #SBATCH --ntasks-per-node=1                # maximum count of tasks per node
 #SBATCH --mail-type=ALL                    # Notify user by email when certain event types occur.
-#SBATCH --gres=gpu:2 # TODO 4
+#SBATCH --gres=gpu:4
 #SBATCH --output=train_%j.txt
 #SBATCH --error=train_%j.txt
 
@@ -18,10 +18,12 @@ cd src/dpo_cluster
 accelerate launch train.py
 cd ../..
 
-python -m src.dpo_cluster.evaluate_model_after_finetuning llama mlpc none
-
-# if the train script is successful, then the next step is to generate again
 if [ $? -eq 0 ]; then
-    cd src/dpo_cluster
-    # TODO reactivate sbatch generate.sh
+    python -m src.dpo_cluster.evaluate_model_after_finetuning mlpc
+
+    # if the train script is successful, then the next step is to generate again
+    if [ $? -eq 0 ]; then
+        cd src/dpo_cluster
+        # TODO reactivate sbatch generate.sh
+    fi
 fi
