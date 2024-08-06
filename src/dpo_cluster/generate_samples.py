4b3c06df588077dfe0d98174d5e64bdd9abb1489
Bertil Braun <bertil.braun.private@gmail.com>
2024-08-06 13:16:23 +0200

    Loading the current model during generation

--- src/dpo_cluster/generate_samples.py
+++ src/dpo_cluster/generate_samples.py
@@ -68,7 +68,7 @@ def generate_sample(index: int, samples_to_generate: list[SampleToGenerate]) -> 
     tokenizer = get_tokenizer()
     with timeblock(f'Loading model on {index}'):
         model = get_model(
-            BASE_MODEL_ID,  # TODO This should be the current model but for some reason it is not loading (at least not within 30min)
+            # BASE_MODEL_ID,  # TODO This should be the current model but for some reason it is not loading (at least not within 30min)
             device=f'cuda:{index}',
             load_in_8bit=True,
         )
