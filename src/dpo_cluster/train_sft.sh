17201d58252de9a1690806897d65dad644ac4820
Bertil Braun <bertil.braun.private@gmail.com>
2024-08-10 09:45:17 +0200

    Increased SFT Train time

--- src/dpo_cluster/train_sft.sh
+++ src/dpo_cluster/train_sft.sh
@@ -2,7 +2,7 @@
 
 #SBATCH --job-name=train                   # job name
 #SBATCH --partition=gpu_4_a100             # mby GPU queue for the resource allocation.
-#SBATCH --time=08:00:00                    # wall-clock time limit 
+#SBATCH --time=11:00:00                    # wall-clock time limit 
 #SBATCH --mem=100000                       # memory per node
 #SBATCH --nodes=1                          # number of nodes to be used
 #SBATCH --cpus-per-task=4                  # number of CPUs required per MPI task
