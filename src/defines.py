5a1f32a08bef6a90c67757345b3c8096cbb3b9ed
Bertil Braun <bertil.braun.private@gmail.com>
2025-02-21 18:17:20 +0000

    API Key Management

--- src/defines.py
+++ src/defines.py
@@ -3,16 +3,25 @@ import dotenv
 
 dotenv.load_dotenv()
 
-OPENAI_API_KEY = os.getenv('OPENAI_API_KEY', 'sk-...')
-JSONBIN_API_KEY = os.getenv('JSONBIN_API_KEY', '$2a$...')
-JSONBIN_API_KEY = '$2a$10$e5lgWuePR6yPBPIBgN8fb.iIRqRnYXQ9mv2iUL3466zUz0p9CMeVe'
+
+def __get(name: str, default: str | None = None) -> str:
+    value = os.getenv(name)
+    if value is None:
+        if default is not None:
+            return default
+        raise ValueError(f'Please set the {name} environment variable')
+    return value
+
+
+OPENAI_API_KEY = __get('OPENAI_API_KEY')
+JSONBIN_API_KEY = __get('JSONBIN_API_KEY')
+GROQ_API_KEY = __get('GROQ_API_KEY')
+GOOGLE_API_KEY = __get('GOOGLE_API_KEY')
 
 GROQ_BASE_URL = 'https://api.groq.com/openai/v1'
-GROQ_API_KEY = 'gsk_...'
-GROQ_API_KEY = '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'
+GOOGLE_BASE_URL = 'https://generativelanguage.googleapis.com/v1beta/openai/'
 
 os.environ['OPENAI_API_KEY'] = OPENAI_API_KEY
-
 
 DEBUG = False  # Set to True to enable debugging output (streaming of the AI's output to the console)
 MAX_RETRIES = 1
@@ -22,10 +31,8 @@ LOCAL_AI_CODER = 'http://coder.aifb.kit.edu:8080'
 LOCAL_AI_ML_PC = 'http://mlpc.coder.aifb.kit.edu:8080'
 LOCAL_AI_ML_PC = 'http://aifb-bis-gpu01.aifb.kit.edu:8080'
 
-
 # BASE_URL_LLM = None # When using the OpenAI API instead of the Local AI
 BASE_URL_LLM = LOCAL_AI_ML_PC
-
 
 BASE_URL_LLM = GROQ_BASE_URL
 OPENAI_API_KEY = GROQ_API_KEY
